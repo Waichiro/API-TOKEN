6a06441260d94905a20e524eebac1d03d83bd31a
Gabriel Santos <gabrielsantos98898@gmail.com>
2021-11-03 13:49:33 -0300

    login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-
+ 
 
 const jwtSecret = "asfdwçobnlllllahyuhnbflakjshflksadsadasa";
 
